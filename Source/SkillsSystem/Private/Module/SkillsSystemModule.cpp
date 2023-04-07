94180db91b6865f3042116bc36b9d3a651b72a31
shenkns <shenkns@gmail.com>
2023-04-07 23:26:20 +0300

    Module Class

---
+++ Source/SkillsSystem/Private/Module/SkillsSystemModule.cpp
@@ -0,0 +1,7 @@
+// Copyright shenkns Skills System Developed With Unreal Engine. All Rights Reserved 2023.
+
+#include "Module/SkillsSystemModule.h"
+
+IMPLEMENT_MODULE(FSkillsSystemModule, SkillsSystem)
+
+SKILLSSYSTEM_API DEFINE_LOG_CATEGORY(LogSkillsSystem);
---
+++ Source/SkillsSystem/Public/Module/SkillsSystemModule.h
@@ -0,0 +1,11 @@
+// Copyright shenkns Skills System Developed With Unreal Engine. All Rights Reserved 2023.
+
+#pragma once
+
+#include "Modules/ModuleManager.h"
+
+SKILLSSYSTEM_API DECLARE_LOG_CATEGORY_EXTERN(LogSkillsSystem, Log, All);
+
+class FSkillsSystemModule : public IModuleInterface
+{
+};
